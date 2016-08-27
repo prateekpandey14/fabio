b88cdbaa1441678a2fa4279b66eb6a343a32a5cf
Frank Schroeder <frschroeder@ebay.com>
2016-08-27 13:45:39 +0200

    Issue #126: Add Riemann metrics support

--- metrics/gometrics.go
+++ metrics/gometrics.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	graphite "github.com/cyberdelia/go-metrics-graphite"
+	riemann "github.com/pingles/go-metrics-riemann"
 	statsd "github.com/pubnub/go-metrics-statsd"
 	gm "github.com/rcrowley/go-metrics"
 )
@@ -35,6 +36,17 @@ func gmGraphiteRegistry(prefix, addr string, interval time.Duration) (Registry, 
 
 	r := gm.NewRegistry()
 	go graphite.Graphite(r, interval, prefix, a)
+	return &gmRegistry{r}, nil
+}
+
+// gmRiemannRegistry returns a go-metrics registry that reports to a Riemann server.
+func gmRiemannRegistry(prefix, addr string, interval time.Duration) (Registry, error) {
+	if addr == "" {
+		return nil, errors.New("metrics: riemann addr missing")
+	}
+
+	r := gm.NewRegistry()
+	go riemann.Report(r, interval, addr)
 	return &gmRegistry{r}, nil
 }
 
